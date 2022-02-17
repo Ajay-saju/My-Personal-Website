96c6152c7ba528ee0879d46686549bd5a468940c
Ajay-saju <96161265+Ajay-saju@users.noreply.github.com>
2022-02-18 01:10:57 +0530

    Add files via upload

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,32 @@
+// /**
+// * PHP Email Form Validation - v3.2
+// * URL: https://bootstrapmade.com/php-email-form/
+// * Author: BootstrapMade.com
+// */
+
+
+// // function validate(){
+// //    var name = document.getElementById("name");
+// //    var email = document.getElementById("email");
+// //    var subject = document.getElementById("name");
+    
+   
+// //    if(name.value == null || name.value == "" && name.value != Number ){
+// //     alert("Must have a user name ");             //alert for user
+// //     document.form1.username.focus();          //focus method fous the field
+// //     return false;
+// //    }
+    
+// //    if(email.value == null || email.value == "" ){
+// //     alert("Please enter email-id ");             //alert for user
+// //     document.form1.username.focus();          //focus method fous the field
+// //     return false;
+// //    }
+
+// //    if(subject.value == null || subject.value == "" ){
+// //     alert("Please add your subject ");             //alert for user
+// //     document.form1.username.focus();          //focus method fous the field
+// //     return false;
+// //    }
+// //    return true;
+// }
